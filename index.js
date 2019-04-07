9bb795c51c86e81a3c6802da8d50abaf9cbad960
virgincodes <somto.achu5@gmail.com>
2019-04-07 17:37:20 +0100

    linear regression version 2

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ class Savage {
   Linear_Regression(data, label, lossfunction) {
     data = math.matrix(data)
     let dimensions = math.size(data).valueOf();
-    if(dimensions.length > 1){
-      dimensions = dimensions[1]+2
+    if (dimensions.length > 1) {
+      dimensions = dimensions[1] + 1
     }
-    else{
-      dimensions = 3
+    else {
+      dimensions = 2
     }
     // i need to get the dimension of this so i can know haw many params am using
 
@@ -25,10 +25,17 @@ class Savage {
       'y': label
     }
 
-    console.log(dimensions);
-    
 
-    return this.optimizers('gradient_descent', data, dimensions)
+    let expression = ''
+    for (let i = 1; i < dimensions; i++) {
+      expression = expression + 'x' + i + ' * w' + i + ' + '  //x being the variabe w being the weight
+    }
+    expression = expression + 'c'
+    console.log(expression);
+
+
+
+    return this.optimizers('gradient_descent', data, dimensions, expression)
   }
 
 
@@ -42,14 +49,15 @@ class Savage {
     return math.divide(math.subtract(data, math.min(data)), math.subtract(math.max(data), math.min(data)))
   }
 
-  optimizers(optimizer, data, dimensions) {
+  optimizers(optimizer, data, dimensions, expression) {
     const this_ = this
     const optimer_dic = {
+
       'gradient_descent': function () {
-        let element = math.multiply(0, math.range(1, dimensions)).valueOf()
+        let element = math.multiply(0, math.range(0, dimensions)).valueOf()
 
         for (let i = 0; i < 1000; i++) {
-          element = this_.loss_functions()['mse']['compute_gradient'](element, data, 0.01)
+          element = this_.loss_functions()['mse']['compute_gradient'](element, data, 0.01, expression, dimensions)
         }
 
 
@@ -79,11 +87,55 @@ class Savage {
           //me an squared error
           return math.divide(math.square(math.subtract(predicted, real_value)), data.lenth)
         },
-        'compute_gradient': function (params2, data, lr) {
-          var params = math.multiply(0,params2 ).valueOf();
+        'compute_gradient': function (params2, data, lr, expression, dimensions) {
+          const h = math.parse(expression)
+
+          let expressions_list = []
+          for (let i = 1; i < dimensions; i++) {
+            const dh = math.derivative(h, 'w' + i)
+            const new_ = math.parse('(2 / n) * ( ' + dh.toString() + ' * ( y - ( ' + expression + ' )  ) )')
+            // console.log(new_.toString());
+            expressions_list.push(new_)
+          }
+          const dh = math.derivative(h, 'c')
+          const new_ = math.parse('(2 / n) * ( ' + dh.toString() + ' * ( y - ( ' + expression + ' )  ) )')
+          // console.log(new_.toString());
+          expressions_list.push(new_)
+          // console.log(h.eval({ 'x1': 3,'w1':5,'c':6 }));
+
+
+
+          var params = math.multiply(0, params2).valueOf();
           for (var i = 0; i < data.x.length; i++) {
-            params[0] = params[0] - 2 / data.x.length * data.x[i] * (data.y[i] - ((params2[0] * data.x[i]) + params2[1]))
-            params[1] = params[1] - 2 / data.x.length * (data.y[i] - ((params2[0] * data.x[i]) + params2[1]))
+            // params[0] = params[0] - 2 / data.x.length * data.x[i] * (data.y[i] - ((params2[0] * data.x[i]) + params2[1]))
+            // params[1] = params[1] - 2 / data.x.length * (data.y[i] - ((params2[0] * data.x[i]) + params2[1]))
+
+            for (let j = 0; j < dimensions-1; j++) {
+
+              const x = 'x' + (j+1)
+              const w = 'w' + (j+1)
+              let d = {}
+
+              d[x] = data.x[i]
+              d['n'] = data.x.length
+              d[w] = params2[0]
+              d['c'] = params2[1]
+              d['y'] = data.y[i]
+              // console.log(d);
+              
+              params[j] = params[j] - expressions_list[j].eval(d).valueOf()
+
+            }
+
+            
+            params[dimensions - 1] = params[dimensions - 1] - expressions_list[dimensions - 1].eval({
+              n: data.x.length,
+              x1: data.x[i],
+              w1: params2[0],
+              c: params2[1],
+              y: data.y[i]
+            }).valueOf()
+
           }
           params[0] = params2[0] - (lr * params[0]);
           params[1] = params2[1] - (lr * params[1]);
