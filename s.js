ae8202f6469944c369948d4b0d29495d456575e2
virgincodes <somto.achu5@gmail.com>
2019-04-08 18:45:22 +0100

    added multivariant linear regression

--- s.js
+++ s.js
@@ -11,6 +11,19 @@ var data = {
     y: [0, 1, 0, 1, 0, 1, 0, 1]
 }
 
+// var data = {
+//     x:[[0],
+//         [1],
+//         [2],
+//         [3],
+//         [4],
+//         [5],
+//         [6],
+//         [7],
+//         [8],
+//         [9]],
+//     y:[0,1,2,3,4,5,6,7,8,9]
+// }
 let savage_ = new savage()
 var linear = savage_.linearRegression(data['x'], data['y'], 'mse')
 console.log(linear);
